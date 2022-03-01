a600ef9698a0c07a5c26967140ae700aea4f5f79
Dennis Chen <c.w.dennis@gmail.com>
2022-03-01 10:03:59 -0800

    Update rgb default dennis

--- keyboards/sofle/keymaps/rgb_default_dennis/config.h
+++ keyboards/sofle/keymaps/rgb_default_dennis/config.h
@@ -49,8 +49,8 @@
 
 
 #ifdef RGB_MATRIX_ENABLE
-#define RGBLED_NUM 35    // Number of LEDs
-#define RGBLED_NUM 35    // Number of LEDs
+#define RGBLED_NUM 36    // Number of LEDs
+#define RGBLED_NUM 36    // Number of LEDs
 #define DRIVER_LED_TOTAL RGBLED_NUM
 #endif
 
@@ -59,8 +59,8 @@
 
     //#define RGBLIGHT_ANIMATIONS
 	//#define RGBLIGHT_EFFECT_BREATHING
-	#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-	//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+	// #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+	#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 	//#define RGBLIGHT_EFFECT_SNAKE
 	//#define RGBLIGHT_EFFECT_KNIGHT
 	//#define RGBLIGHT_EFFECT_CHRISTMAS
@@ -69,9 +69,9 @@
 	//#define RGBLIGHT_EFFECT_ALTERNATING
 	//#define RGBLIGHT_EFFECT_TWINKLE
 
-    #define RGBLED_NUM 70
+    #define RGBLED_NUM 72
 	//#define RGBLED_SPLIT
-	#define RGBLED_SPLIT { 35, 35 } // haven't figured out how to use this yet
+	#define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
 
 	//#define RGBLED_NUM 30
     #define RGBLIGHT_LIMIT_VAL 120
